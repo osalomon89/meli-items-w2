1ddde651071e8bab054dd85110ab9d9064b92a49
hesuarez <henry.suarez@mercadolibre.com.co>
2023-04-11 08:14:16 -0500

    Api rest finalizado, falta orden por fecha de actualización

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"apiRestPractice/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	controller.DataMock()
+	server := gin.Default()
+	server.POST("v1/items", controller.AddItem)
+	server.PUT("v1/items/:id", controller.UpdateItem)
+	server.GET("v1/items/:id", controller.GetById)
+	server.DELETE("v1/items/:id", controller.DeleteById)
+	server.GET("v1/items/search", controller.GetByStatusAndLimit)
+	server.Run(":9000")
+}
